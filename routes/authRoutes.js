fe1758ec7479d82d6c77d32c61e50e58febb9128
Lakshdeep Singh <lakshdeep.code@gmail.com>
2024-07-06 14:21:59 +0530

    modified authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import { loginUser, signupUser} from "../controllers/authController.js";
+import { loginUser, signupUser, refreshToken} from "../controllers/authController.js";
 
 const authRouter = new Router();
 
